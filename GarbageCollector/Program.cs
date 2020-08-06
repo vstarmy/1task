204a3ec8cb7fa213b154460ef2ecf302e5252a0f
vstarmy <vicundoska@gmail.com>
2020-08-06 19:55:33 +0400

    Сборщик мусора

---
+++ GarbageCollector/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GarbageCollector
+{
+    class Program
+    {
+        static void Main()
+        {
+            string gl = "A";
+            while (gl.Length>=1)
+            {
+                gl +=gl;
+                Console.WriteLine(gl.Length);
+            }
+            Console.ReadKey();
+        }
+    }
+}
